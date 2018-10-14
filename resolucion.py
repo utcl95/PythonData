1dd247ff6a19f2b38940d9c4245ce33d0986a65d
Juan Aquino <utcl95@gmail.com>
2018-10-14 00:32:01 -0500

    Crear nueva columna usando una funcion.

--- resolucion.py
+++ resolucion.py
@@ -5,35 +5,42 @@ import pandas as pd
 
 __author__ = 'jaquinoa'
 
-db_engine = create_engine('mysql+mysqlconnector://root:a123456B@localhost/eel')
+def split_rfecha(rd):
+    s = rd.split('-')
+    r = '000' + s[0]
+    r = '2018_' + r[-5:]
+    return r
+
+db_engine = create_engine('mysql+pymysql://root:a123456B@localhost/pydb')
 
 parser = argparse.ArgumentParser(description='Copiar Resoluciones en Excel a DB')
 parser.add_argument('-e', '--excel', help='Archivo Excel', required=True)
 parser.add_argument('-s', '--sheet', help='Hoja Excel con las Resoluciones', required=True)
 args = parser.parse_args()
 
-# Load Excel File
+print("**** Copiar Excel resoluciones a una Tabla MySQL ****")
+
+print("Leyendo archivo Excel...")
 xlsfile = args.excel
 sheet   = args.sheet
-df =  pd.read_excel(xlsfile, sheet_name = sheet)
+df =  pd.read_excel(xlsfile, sheet_name = sheet, usecols = 10)
 
-# Drop empty rows
+print("Borrar filas vacias...")
 df.dropna(subset=['R.D.UGEL'], inplace=True)
 
 # Set columns names
 df.columns = ['Apellidos','rd', 'Fecha', 'Asunto', 'ie', 'Expediente', 'Folios', 'Proyecto', 'Area', 'DNI', 'Otro']
 
+
 # Dates
 df["Fecha"] = df["Fecha"].dt.strftime("%d/%m/%Y")
 
 # rfecha
-df["rfecha"] = "2018_" + df["rd"].str[:4]
+df["rfecha"] = df["rd"].apply(split_rfecha)
 
-#print(df.head())
-#print(df["rfecha"])
+print(df.head())
 
-print("Hello World...")
-# Copy to Table
-# df.to_sql(con=db_engine, name='_resoluciones2018', if_exists='replace')
+print("Copiar a Base de Datos...")
+df.to_sql(con=db_engine, name='_resoluciones2018', if_exists='replace', index_label = 'id')
 
 print('Done...')
